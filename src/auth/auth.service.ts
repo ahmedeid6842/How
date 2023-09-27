cffacf6b747cdc72d83febf80d837806aff9185e
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-27 06:12:41 +0300

    Implement AuthSerivce.register to create a unique user with hashed password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+    constructor(private userService: UsersService) { }
+
     async register(email: string, userName: string, password: string) {
-        return "User registered";
+        // check if user email is unique
+        const userByEmail = await this.userService.find(email);
+        if (userByEmail) {
+            throw new BadRequestException(`this email:${email} already exists`);
+        }
+
+        // check if user userName is unique 
+        const userByName = await this.userService.find(null, userName);
+        if (userByName) {
+            throw new BadRequestException(`this userName: ${userName} already exist`)
+        }
+
+        const salt = await bcrypt.genSalt()
+        password = await bcrypt.hash(password, salt);
+
+        const user = await this.userService.create(email, userName, password);
+        return user;
     }
 }
