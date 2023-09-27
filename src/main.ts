d78dff3a9f64554d5954efcad1b6ef05447e095c
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-27 06:38:52 +0300

    Setup the cookie session to be used

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ["aKeyForTheCookieSession"]
+  }))
   await app.listen(3000);
 }
 bootstrap();
