35e9d69eaf9a579a74cf29600ba006b950b50821
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-27 06:57:59 +0300

    Fixed a bug of check if the user exists or not at the register
    Bug Reason: find method return array if a user found or not ,
                so in case if no user found it will be empty array but not null
                which lead if the user duplicated or not the condition `if(userByEmail)`will consider true
    
    Bug Solution: check the array length rather than array existance or not.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,13 +9,13 @@ export class AuthService {
     async register(email: string, userName: string, password: string) {
         // check if user email is unique
         const userByEmail = await this.userService.find(email);
-        if (userByEmail) {
+        if (userByEmail.length) {
             throw new BadRequestException(`this email:${email} already exists`);
         }
 
         // check if user userName is unique 
         const userByName = await this.userService.find(null, userName);
-        if (userByName) {
+        if (userByName.length) {
             throw new BadRequestException(`this userName: ${userName} already exist`)
         }
 
