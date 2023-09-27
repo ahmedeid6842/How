9fed95aa9126273242ff9be562df37746194c6a5
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-27 07:42:48 +0300

    Implement the Serialize decorator to simplify the use of SerializeInterceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,6 +1,16 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from "@nestjs/common";
 import { plainToInstance } from "class-transformer";
 import { Observable, map } from "rxjs";
+
+
+interface ClassConstructor {
+    // sepcify it will be an class
+    new(...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) { // implement a decorator for serialize to simplify the Interceptors call
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
     constructor(private dto: any) { }
