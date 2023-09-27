a28416cecb2a64f7c1de4577d5aea6807ca4eca7
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-27 05:13:05 +0300

    Implement the register controller and assign it to authService method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService) { }
+
+    @Post('/register')
+    async createUser(@Body() body: any) {
+        const user = await this.authService.register(body.email, body.userName, body.password);
+
+        return user;
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    async register(email: string, userName: string, password: string) {
+        return "User registered";
+    }
+}
