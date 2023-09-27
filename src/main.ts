5942fe83a1893c3d61805c0d2c303a76efc68d78
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-27 06:40:14 +0300

    setup the validationPipe as a global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieSession({
     keys: ["aKeyForTheCookieSession"]
+  }))
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true // set the white list to true to execlude the unspecified properties
   }))
   await app.listen(3000);
 }
