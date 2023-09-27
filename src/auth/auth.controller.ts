80453248e593a1f4592320bfe16b0a5b38bd0736
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-27 06:30:49 +0300

    Added dto for user creation and add it to register controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('/register')
-    async createUser(@Body() body: any) {
+    async createUser(@Body() body: CreateUserDto) {
         const user = await this.authService.register(body.email, body.userName, body.password);
 
         return user;
