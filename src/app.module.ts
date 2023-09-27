82e95fd6ba55738a042a6a8b655af0b538cef38c
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-27 03:32:53 +0300

    Setup postgres connection to How DB and user 'postgres'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm"
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'root',
+    database: 'How',
+    entities: [],
+    synchronize: process.env.NODE_ENV == 'development' ? true : false
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
