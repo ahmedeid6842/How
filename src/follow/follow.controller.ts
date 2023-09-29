d50f7138e2db873f0e1be052481874c806c9656e
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-29 10:45:54 +0300

    Use the followDto to serialize the outgoing responses instead of userDto

--- src/follow/follow.controller.ts
+++ src/follow/follow.controller.ts
@@ -1,21 +1,21 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { FollowService } from './follow.service';
-import { User } from 'src/auth/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
-import { UserDto } from 'src/auth/dto/user.dto';
+import { Follow } from './follow.entity';
+import { FollowDto } from './dto/follow.dto';
 
-@Serialize(UserDto)
+@Serialize(FollowDto)
 @Controller('follow')
 export class FollowController {
   constructor(private readonly followersService: FollowService) { }
 
   @Get('/followers/:id')
-  async getUserFollowers(@Param('id') userId: number): Promise<User[]> {
+  async getUserFollowers(@Param('id') userId: number): Promise<Follow[]> {
     return this.followersService.getUserFollowers(userId);
   }
 
   @Get('/following/:id')
-  async getUserFollowing(@Param('id') userId: number): Promise<User[]> {
+  async getUserFollowing(@Param('id') userId: number): Promise<Follow[]> {
     return this.followersService.getUserFollowing(userId);
   }
 
