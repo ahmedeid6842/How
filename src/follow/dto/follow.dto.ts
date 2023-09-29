6b565e2a866df5d5fa84352f9f6658b335a2e114
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-29 10:43:56 +0300

    Implement FollowDto for outgoing responses

---
+++ src/follow/dto/follow.dto.ts
@@ -0,0 +1,16 @@
+import { Exclude, Expose, Type } from 'class-transformer';
+import { UserDto } from 'src/auth/dto/user.dto';
+
+
+export class FollowDto {
+    @Expose()
+    id: string;
+
+    @Expose()
+    @Type(() => UserDto)
+    follower: UserDto;
+
+    @Expose()
+    @Type(() => UserDto)
+    user: UserDto;
+}
