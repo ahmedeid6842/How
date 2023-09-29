e10f0fb0436e53bfa38eb1e2f77acfee5789b9bf
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-29 10:03:37 +0300

    Added the Follow entity to the follow module

--- src/follow/follow.module.ts
+++ src/follow/follow.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FollowController } from './follow.controller';
 import { FollowService } from './follow.service';
+import { Follow } from './follow.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Follow])],
   controllers: [FollowController],
   providers: [FollowService]
 })
