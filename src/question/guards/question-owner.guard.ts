03e73891ead28a2876aed631c1c76246a137973a
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-02 07:22:55 +0300

    check if the question is exist or not.

--- src/question/guards/question-owner.guard.ts
+++ src/question/guards/question-owner.guard.ts
@@ -1,26 +1,30 @@
-import { BadRequestException, CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { QuestionService } from '../question.service';
 
 @Injectable()
 export class QuestionOwnerGuard implements CanActivate {
-  constructor(private questionService: QuestionService) {}
+    constructor(private questionService: QuestionService) { }
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const { questionId } = request.params;
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const { questionId } = request.params;
 
-    if (!questionId) {
-      throw new BadRequestException('No question id provided');
+        if (!questionId) {
+            throw new BadRequestException('No question id provided');
+        }
+
+        const [question] = await this.questionService.getQuestion({ questionId });
+
+        if (!question) {
+            throw new NotFoundException("Not question found with the given ID")
+        }
+        
+        if (question.author.id !== request.currentUser.id) {
+            throw new UnauthorizedException("You aren't the owner of this question");
+        }
+
+        request.ownerQuestion = question;
+
+        return true;
     }
-
-    const [question] = await this.questionService.getQuestion({ questionId });
-
-    if (question.author.id !== request.currentUser.id) {
-      throw new UnauthorizedException("You aren't the owner of this question");
-    }
-
-    request.ownerQuestion = question;
-
-    return true;
-  }
 }
