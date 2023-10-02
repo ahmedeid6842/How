f55e30d6f1793f4a1ec86dc852cc8a04a82cb8db
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-02 04:19:41 +0300

    Implement create question dto & use AuthGurad for createQuestion controller

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { User } from 'src/auth/user.entity';
 import { QuestionService } from './question.service';
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('question')
 export class QuestionController {
     constructor(private questionService: QuestionService) { }
 
+    @UseGuards(AuthGuard)
     @Post("/")
-    createQuestion(@Body() body, @CurrentUser() user: User) {
+    createQuestion(@Body() body: CreateQuestionDto, @CurrentUser() user: User) {
         this.questionService.addQuestion(body, user);
     }
 }
