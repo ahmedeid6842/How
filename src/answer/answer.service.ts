e69e3765cfccc7659013c5fd7df29da034729add
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-05 06:06:23 +0300

    Implement create an answer service method

--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -1,13 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from 'src/auth/user.entity';
 import { CreateAnswernDto } from './dto/create-answer.dto';
 import { QuestionService } from 'src/question/question.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Answer } from './answer.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AnswerService {
-    constructor(private readonly questionService: QuestionService) { }
-    
-    createAnswer(questionId: string, body: CreateAnswernDto, user: User) {
-        return "answer created"
+    constructor(
+        private readonly questionService: QuestionService,
+        @InjectRepository(Answer) private readonly answerRepository: Repository<Answer>
+    ) { }
+
+    async createAnswer(questionId: string, body: CreateAnswernDto, user: User) {
+        const [questionExist] = await this.questionService.getQuestion({ questionId })
+        if (!questionExist) {
+            throw new NotFoundException("No question found with the given ID ")
+        }
+
+        const savedAnswer = await this.answerRepository.create({
+            answer: body.answer,
+            question: questionExist,
+            respondent: user
+        })
+
+        await this.answerRepository.save(savedAnswer)
     }
 }
