bf667b5c4e1663ac644f59db0259303195e6de57
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-05 05:40:36 +0300

    Implement create an anwer controller

--- src/answer/answer.controller.ts
+++ src/answer/answer.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { AnswerService } from './answer.service';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { User } from 'src/auth/user.entity';
+import { CreateAnswernDto } from './dto/create-answer.dto';
 
 @Controller('answer')
-export class AnswerController {}
+export class AnswerController {
+    constructor(private answerSerivce: AnswerService) { }
+
+    @UseGuards(AuthGuard)
+    @Post("/:questionId")
+    async createAnswer(@Param('questionId') questionId: string, @Body() body: CreateAnswernDto, @CurrentUser() user: User) {
+        await this.answerSerivce.createAnswer(questionId, body, user)
+    }
+}
--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/auth/user.entity';
+import { CreateAnswernDto } from './dto/create-answer.dto';
 
 @Injectable()
-export class AnswerService {}
+export class AnswerService {
+    createAnswer(questionId: string, body: CreateAnswernDto, user: User) {
+        return "answer created"
+    }
+}
