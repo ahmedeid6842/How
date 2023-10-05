77badbaec698862f8e0f629007f0f581c2b1b651
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-06 01:24:27 +0300

    Implement AnswerLikes service

--- src/answer/answer.module.ts
+++ src/answer/answer.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Answer } from './answer.entity';
 import { QuestionModule } from 'src/question/question.module';
 import { AnswerLikes } from './answer-likes.entity';
+import { AnswerLikesService } from './answer-likes.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Answer,AnswerLikes]),QuestionModule],
+  imports: [TypeOrmModule.forFeature([Answer, AnswerLikes]), QuestionModule],
   controllers: [AnswerController],
-  providers: [AnswerService]
+  providers: [AnswerService, AnswerLikesService]
 })
 export class AnswerModule { }
