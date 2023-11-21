a4d88e6107f7ec64120c18b5a16e16e84e71f4be
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-11-21 21:03:51 +0200

    feat: Add class-transformer to pagination dto for input transformation
    This commit adds the class-transformer package to the pagination.dto.ts
    file. It also includes a transform function that parses the input value
    into an integer. This change enables the transformation of the input
    values for the page and limit fields.

--- src/answer/dto/pagination.dto.ts
+++ src/answer/dto/pagination.dto.ts
@@ -1,12 +1,15 @@
+import { Transform } from "class-transformer";
 import { IsOptional, IsInt, Min } from "class-validator"
 
 export class PaginationDto {
     @IsOptional()
+    @Transform(({ value }) => parseInt(value))
     @IsInt()
     @Min(1)
     page?: number = 1;
 
     @IsOptional()
+    @Transform(({ value }) => parseInt(value))
     @IsInt()
     @Min(1)
     limit?: number = 1;
