b49c1f8af77f5d6cd2516c4b34b96580e067771f
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-03 00:10:27 +0200

    Add BasePartialProfileDto as a base dto and extend it

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,35 +1,9 @@
 import { ArrayMaxSize, ArrayMinSize, ArrayUnique, IsArray, IsBoolean, IsOptional, IsString, Length } from "class-validator";
 import { UserDto } from "src/auth/dto/user.dto";
+import { BasePartialProfileDto } from "./base-partial-profile.dto";
 
-export class CreateProfileDto {
+export class CreateProfileDto extends BasePartialProfileDto {
     @IsString({ message: 'name must be a string' })
     @Length(5, 100, { message: 'name must be between 5 and 100 characters' })
     name: string;
-
-    @IsOptional()
-    @IsString({ message: 'Bio must be a string' })
-    @Length(10, 500, { message: 'Bio must be between 10 and 500 characters' })
-    bio?: string;
-
-    @IsOptional()
-    @IsArray({ message: 'Social links must be an array' })
-    @ArrayMinSize(1, { message: 'At least one social link is required' })
-    @ArrayMaxSize(5, { message: 'Maximum of 5 social links allowed' })
-    @ArrayUnique((link: string) => link.toLowerCase(), {
-        message: 'Social links must be unique (case insensitive)',
-    })
-    socialLinks?: string[];
-
-    @IsOptional()
-    @IsArray({ message: 'Interests must be an array' })
-    @ArrayMinSize(3, { message: 'At least three interests are required' })
-    @ArrayMaxSize(10, { message: 'Maximum of 10 interests allowed' })
-    @ArrayUnique((interest: string) => interest.toLowerCase(), {
-        message: 'Interests must be unique (case insensitive)',
-    })
-    interests?: string[];
-
-    @IsOptional()
-    @IsBoolean()
-    isPublic?: boolean;
 }
