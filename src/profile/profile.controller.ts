89c1fc7c1c174551873a7340183f658ba6c9f413
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-03 00:12:48 +0200

    Add updateProfile endpoint to ProfileController

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Patch, UseGuards } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { ProfileService } from './profile.service';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { User } from 'src/auth/user.entity';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profile')
-export class ProfileController {}
+export class ProfileController {
+    constructor(private profileService: ProfileService) { }
+
+    @Patch()
+    @UseGuards(AuthGuard)
+    async updateProfile(@Body() body: UpdateProfileDto, @CurrentUser() user: User) {
+        return await this.profileService.updateProfile(user.id, body);
+    }
+}
