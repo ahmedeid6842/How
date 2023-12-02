f0b2edfb3661c77797d2f57f5d30596d69d168c0
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-12-02 06:22:56 +0200

    Add createProfile method to ProfileService

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Profile } from './profile.entity';
+import { Repository } from 'typeorm';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+    constructor(@InjectRepository(Profile) private profileRepo: Repository<Profile>) { }
+
+    createProfile(profile: CreateProfileDto, user: User): Promise<Profile> {
+        const newProfile = this.profileRepo.create({ ...profile, user });
+
+        return this.profileRepo.save(newProfile);
+    }
+}
